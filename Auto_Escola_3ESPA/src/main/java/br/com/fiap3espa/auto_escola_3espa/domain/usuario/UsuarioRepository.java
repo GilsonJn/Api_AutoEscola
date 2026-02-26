3669071c45afd09abda4adfff8f97fe3e0858310
Gilson Dias <gilsondiasjn@gmail.com>
2026-02-26 11:49:20 -0300

    .

---
+++ Auto_Escola_3ESPA/src/main/java/br/com/fiap3espa/auto_escola_3espa/domain/usuario/UsuarioRepository.java
@@ -0,0 +1,8 @@
+package br.com.fiap3espa.auto_escola_3espa.domain.usuario;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+    UserDetails findByLogin(String login);
+}
